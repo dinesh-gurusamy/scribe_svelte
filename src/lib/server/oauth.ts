5595e32258bd17c283d1c1d008833bd323ac1e7e
unknown <dinesh-gurusamy>
2025-10-03 15:19:05 +0530

    updated

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,8 +1,13 @@
 import { Google } from "arctic";
 import { env } from "$env/dynamic/private";
 
+
+const callbackURL = import.meta.env.PROD
+  ? "https://scribe-svelte.vercel.app//login/google/callback"
+  : "http://localhost:5173/login/google/callback";
+
 export const google = new Google(
   env.GOOGLE_CLIENT_ID as string,
   env.GOOGLE_CLIENT_SECRET as string,
-  "http://localhost:5173/login/google/callback" // replace in prod
+  callbackURL
 );
